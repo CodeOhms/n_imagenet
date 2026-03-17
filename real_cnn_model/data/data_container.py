d41810c66d1f3f0c08633de445aaeeb6f4cbe297
CodeOhms <27335964+CodeOhms@users.noreply.github.com>
2026-03-17 14:29:21 +1000

    Enable the gen_dataloader method of ImageNetContainer to use the kwargs passed to it, and pass it along to the Pytorch DataLoader.

--- real_cnn_model/data/data_container.py
+++ real_cnn_model/data/data_container.py
@@ -36,36 +36,60 @@ class ImageNetContainer(DataContainer):
             else:
                 raise AttributeError('Mode not provided')
 
+    def _handle_dataloader_kwargs(self, **kwargs):
+        dataloader_def_kwargs = {
+            "collate_fn": self.dict_collate_fn,
+            "batch_size": self.cfg.batch_size,
+            "num_workers": self.cfg.num_workers,
+            "drop_last": False,
+            "pin_memory": self.cfg.pin_memory
+        }
+        for kword, default_karg in dataloader_def_kwargs.items():
+            if kword not in kwargs.keys():
+                kwargs[kword] = default_karg
+
+    def gen_train_dataloader(self, **kwargs):
+        assert self.dataloader is not None
+        self._handle_dataloader_kwargs(**kwargs)
+        kwargs['shuffle'] = True
+        self.dataloader['train'] = DataLoader(self.dataset['train'], **kwargs)
+
+    def gen_val_dataloader(self, **kwargs):
+        assert self.dataloader is not None
+        self._handle_dataloader_kwargs(**kwargs)
+        kwargs['shuffle'] = True
+        self.dataloader['val'] = DataLoader(self.dataset['val'], **kwargs)
+
+    def gen_test_dataloader(self, **kwargs):
+        assert self.dataloader is not None
+        self._handle_dataloader_kwargs(**kwargs)
+        kwargs['shuffle'] = False
+        self.dataloader['test'] = DataLoader(self.dataset['test'], **kwargs)
+
     def gen_dataloader(self, **kwargs):
-        # collate_fn for generating dictionary-style batches.
-        def dict_collate_fn(list_data):
-            # list_data is as follows: [(img: torch.Tensor(H, W, 3), event: torch.Tensor(H, W, 4), label: int), ...]
-            collate_type = getattr(self.cfg, 'collate_type', 'normal')
-            if collate_type == 'normal':
-                events, labels = list(zip(*list_data))
+        self.gen_all_dataloaders(**kwargs)
 
-                event_batch = torch.stack(events, dim=0)
-                label_batch = torch.LongTensor(labels)
-                return {
-                    'input_data': event_batch,
-                    'label': label_batch
-                }
-
+    def gen_all_dataloaders(self, train_kwargs={}, val_kwargs={}, test_kwargs={}, **kwargs):
+        super().gen_dataloader()
         assert self.dataloader is not None
         if self.cfg.mode == 'train':
-            TrainLoader = DataLoader(self.dataset['train'], collate_fn=dict_collate_fn,
-                batch_size=self.cfg.batch_size, shuffle=True, num_workers=self.cfg.num_workers, drop_last=False, pin_memory=self.cfg.pin_memory)
-
-            ValLoader = DataLoader(self.dataset['val'], collate_fn=dict_collate_fn,
-                batch_size=self.cfg.batch_size, shuffle=True, num_workers=self.cfg.num_workers, drop_last=False, pin_memory=self.cfg.pin_memory)
-
-            self.dataloader['train'] = TrainLoader
-            self.dataloader['val'] = ValLoader
-
+            self.gen_train_dataloader(**train_kwargs)
+            self.gen_val_dataloader(**val_kwargs)
         elif self.cfg.mode == 'test':
-            TestLoader = DataLoader(self.dataset['test'], collate_fn=dict_collate_fn,
-                batch_size=self.cfg.batch_size, shuffle=False, num_workers=self.cfg.num_workers, drop_last=False, pin_memory=self.cfg.pin_memory)
-            self.dataloader['test'] = TestLoader
-
+            self.gen_test_dataloader(**test_kwargs)
         else:
             raise AttributeError('Mode not provided')
+
+    # collate_fn for generating dictionary-style batches.
+    def dict_collate_fn(self, list_data):
+        # list_data is as follows: [(img: torch.Tensor(H, W, 3), event: torch.Tensor(H, W, 4), label: int), ...]
+        collate_type = getattr(self.cfg, 'collate_type', 'normal')
+        if collate_type == 'normal':
+            events, labels = list(zip(*list_data))
+
+            event_batch = torch.stack(events, dim=0)
+            label_batch = torch.LongTensor(labels)
+            return {
+                'input_data': event_batch,
+                'label': label_batch
+            }
